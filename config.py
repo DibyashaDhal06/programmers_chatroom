ba548f939e8649401d661c3cc9573ab4ddd4f445
supratikchatterjee16 <supratikdevm96@gmail.com>
2020-10-10 21:45:35 +0530

    Sample production ready

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # The following are the configurations for the server. Change at your own peril.
 import os
 db_path = os.path.join(os.getcwd(), 'app.db')
-config = {'BASE_DIR': 'C:\\Users\\supra\\Workbench', 'SQLALCHEMY_DATABASE_URI': 'sqlite:///'+db_path, 'SQLALCHEMY_BINDS': {}, 'SQLALCHEMY_TRACK_MODIFICATIONS': False, 'PERMANANENT_SESSION_LIFETIME': 7776000.0, 'APPLICATION_ROOT': '/', 'PREFERRED_URL_SCHEME': 'https', 'JSON_AS_ASCII': True, 'JSON_SORT_KEYS': True, 'SESSION_COOKIE_NAME': 'localhost.dev', 'TRAP_HTTP_EXCEPTIONS': True, 'CAPTCHA_ENABLE': False, 'MAIL_ENABLE': False, 'LDAP_ENABLE': False, 'SUBNET_MASK': '24'}
+config = {'SQLALCHEMY_DATABASE_URI': 'sqlite:///'+db_path, 'SQLALCHEMY_BINDS': {}, 'SQLALCHEMY_TRACK_MODIFICATIONS': False, 'PERMANANENT_SESSION_LIFETIME': 7776000.0, 'APPLICATION_ROOT': '/', 'PREFERRED_URL_SCHEME': 'https', 'JSON_AS_ASCII': True, 'JSON_SORT_KEYS': True, 'SESSION_COOKIE_NAME': 'programmers-chatroom.herokuapp.com', 'TRAP_HTTP_EXCEPTIONS': True, 'CAPTCHA_ENABLE': False, 'MAIL_ENABLE': False, 'LDAP_ENABLE': False, 'SUBNET_MASK': '24'}
