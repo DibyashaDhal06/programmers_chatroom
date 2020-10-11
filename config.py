6c1dab65ba34bfb87d1ab9e35d2372d383b58985
supratikchatterjee16 <supratikdevm96@gmail.com>
2020-10-11 11:43:17 +0530

    Production server fix

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 # The following are the configurations for the server. Change at your own peril.
 import os
 db_path = os.path.join(os.getcwd(), 'app.db')
-config = {'SQLALCHEMY_DATABASE_URI': 'sqlite:///'+db_path, 'SQLALCHEMY_BINDS': {}, 'SQLALCHEMY_TRACK_MODIFICATIONS': False, 'PERMANANENT_SESSION_LIFETIME': 7776000.0, 'APPLICATION_ROOT': '/', 'PREFERRED_URL_SCHEME': 'https', 'JSON_AS_ASCII': True, 'JSON_SORT_KEYS': True, 'SESSION_COOKIE_NAME': 'programmers-chatroom.herokuapp.com', 'TRAP_HTTP_EXCEPTIONS': True, 'CAPTCHA_ENABLE': False, 'MAIL_ENABLE': False, 'LDAP_ENABLE': False, 'SUBNET_MASK': '24'}
+config = {'SQLALCHEMY_DATABASE_URI': 'sqlite:///'+db_path, 'SQLALCHEMY_BINDS': {}, 'SQLALCHEMY_TRACK_MODIFICATIONS': False, 'PERMANANENT_SESSION_LIFETIME': 7776000.0, 'APPLICATION_ROOT': '/', 'PREFERRED_URL_SCHEME': 'https', 'JSON_AS_ASCII': True, 'JSON_SORT_KEYS': True,
+ 'SESSION_COOKIE_NAME': 'programmers-chatroom.herokuapp.com', # Production
+# 'SESSION_COOKIE_NAME': 'localhost:8000', # Development
+'TRAP_HTTP_EXCEPTIONS': True, 'CAPTCHA_ENABLE': False, 'MAIL_ENABLE': False, 'LDAP_ENABLE': False, 'SUBNET_MASK': '24'}
--- production.py
+++ production.py
@@ -1,6 +1,8 @@
 from app import create
+import waitress
 import os
 if __name__ == '__main__':
 	application = create()
 	port = int(os.environ.get("PORT", 5000))
-	application.run(host='0.0.0.0', port = port, debug = False)
+	#application.run(host='0.0.0.0', port = port, debug = False)
+	waitress.serve(application, host='0.0.0.0', port=port, threads=8, )
